b8f2a42b74729f0c2c5dc0ca477a575661b45d7e
mcnsgit <cardigamiguel221@gmail.com>
2024-01-31 16:47:07 +0000

    c

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
-document.addEventListener("keypress", (event) => {
-    
-    let key = event.key
-    let which = event.which
-    let code = event.code
+document.addEventListener("keydown", (event) => {
+    const welcome = document.getElementById("welcome");
+    const generator = document.getElementById("generator");
 
-    document.getElementById("key").innerHTML = key
-    document.getElementById("which").innerHTML = which
-    document.getElementById("code").innerHTML = code
-})
+    let key = event.key;
+    let keyCode = event.keyCode;
+    let location = getLocationDescription(event);
+    let code = event.code;
+
+    document.getElementById("eventKey").textContent = key;
+    document.getElementById("eventKeyCode").textContent = keyCode;
+    document.getElementById("eventLocation").textContent = location;
+    document.getElementById("eventCode").textContent = code;
+
+    generator.style.display = "block";
+    welcome.style.display = "none";
+});
+
+function getLocationDescription(event) {
+    const keyWithLocation = ['Alt', 'Shift', 'Control', 'Enter', 'Meta', 'ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown', 'NumpadEnter', 'NumpadAdd', 'NumpadSubtract', 'NumpadMultiply', 'NumpadDivide', 'NumpadDecimal'];
+    
+    if (keyWithLocation.includes(event.code)) { 
+        switch (event.location) {
+        case 0:
+            return "Standard";
+        case 1:
+            return "Left";
+        case 2:
+            return "Right";
+        default:
+            return "Unknown";
+    }
+    }
+    else {
+        return "";
+    }
+}
+    
